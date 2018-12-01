39cec51262fc5f5b0f0515f66f20d58b598a5fbd
anthonydenecheau <anthony.denecheau@centrale-canine.fr>
2018-12-01 15:21:04 +0100

    last update

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,2 +1,2 @@
 -- 01/12/2018 : Mantis 0320 : Ajout information Frequence d'utilisation de la lice
-ALTER TABLE ls_stats_eleveur ADD COLUMN IF NOT EXISTS new_lice varchar(1);
+ALTER TABLE IF EXISTS ls_stats_eleveur ADD COLUMN IF NOT EXISTS new_lice varchar(1);
