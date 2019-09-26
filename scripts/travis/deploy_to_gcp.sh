ffced423f4ebaa1dfed5a15860374361f1bc1d90
Ludovic Piot <ludovic.piot@thegaragebandofit.com>
2019-09-26 17:14:01 +0200

    :construction_worker: :recycle: lighten deployment workflow

--- scripts/travis/deploy_to_gcp.sh
+++ scripts/travis/deploy_to_gcp.sh
@@ -19,7 +19,7 @@ cd terraform/code
 echo "update .env file ..."
 sed -i "s/export TAG_SCC_LOFSELECTCLUB_SERVICE=.*/export TAG_SCC_LOFSELECTCLUB_SERVICE=${BUILD_NAME}/" .env 2>&1
 
-echo "update .env file ..."
+echo "push .env file ..."
 
 git config user.email "anthony.denecheau@centrale-canine.fr"
 git config user.name "${GITHUB_USER}"
@@ -27,10 +27,9 @@ git add .
 git commit -m":rocket: :wrench: :arrow_up: changed application version" .
 git push
 
-#echo "push to branch re7..."
-#git checkout -b re7
-#git pull origin re7
-#git merge origin/master
-#
-#git push -u origin re7
+echo "push to branch re7..."
+git checkout --track origin/re7
+git merge master
+
+git push -u origin re7
 
