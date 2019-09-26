e8cd9663e5923375995da502800c06ef97f733a3
Ludovic Piot <ludovic.piot@thegaragebandofit.com>
2019-09-26 17:28:12 +0200

    :construction_worker: :recycle: lighten deployment workflow

--- scripts/travis/deploy_to_gcp.sh
+++ scripts/travis/deploy_to_gcp.sh
@@ -3,16 +3,9 @@
 GITHUB_USER=anthonydenecheau
 GITHUB_REPO=github.com/centrale-canine/gcp-migration.git
 
-if [ "q${GITHUB_BRANCH}" == "q" ]
-then
-    echo "ERROR! A specific branch indicating the environment to deploy to should be specified in the GITHUB_BRANCH env. variable!"
-    exit 400
-fi
-
 echo "clone gcp project..."
-echo "branch: ${GITHUB_BRANCH}"
 
-git clone --single-branch --branch ${GITHUB_BRANCH} https://${GITHUB_USER}:${GITHUB_TOKEN}@${GITHUB_REPO} 2>&1
+git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@${GITHUB_REPO} 2>&1
 
 cd terraform/code
 
@@ -20,7 +13,6 @@ echo "update .env file ..."
 sed -i "s/export TAG_SCC_LOFSELECTCLUB_SERVICE=.*/export TAG_SCC_LOFSELECTCLUB_SERVICE=${BUILD_NAME}/" .env 2>&1
 
 echo "push .env file ..."
-
 git config user.email "anthony.denecheau@centrale-canine.fr"
 git config user.name "${GITHUB_USER}"
 git add .
