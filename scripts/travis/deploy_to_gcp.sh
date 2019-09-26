9e163149a683d8cc3b95a2c4d38085a8a4c315e4
Ludovic Piot <ludovic.piot@thegaragebandofit.com>
2019-09-26 16:22:46 +0200

    :art: :wrench: :construction_worker: added branch dependent deployments into Travis-CI

--- scripts/travis/deploy_to_gcp.sh
+++ scripts/travis/deploy_to_gcp.sh
@@ -1,19 +1,36 @@
+#!/usr/bin/env bash
 
-echo "clone gcp project ..."
-git clone https://anthonydenecheau:${GITHUB_TOKEN}@https://github.com/centrale-canine/gcp-migration.git 2>&1
+GITHUB_USER=anthonydenecheau
+GITHUB_REPO=github.com/centrale-canine/gcp-migration.git
+
+if [ "q${GITHUB_BRANCH}" == "q" ]
+then
+    echo "ERROR! A specific branch indicating the environment to deploy to should be specified in the GITHUB_BRANCH env. variable!"
+    exit 400
+fi
+
+echo "clone gcp project..."
+echo "branch: ${GITHUB_BRANCH}"
+
+git clone --single-branch --branch ${GITHUB_BRANCH} https://${GITHUB_USER}:${GITHUB_TOKEN}@${GITHUB_REPO} 2>&1
+
 cd terraform/code
 
 echo "update .env file ..."
 sed -i "s/export TAG_SCC_LOFSELECTCLUB_SERVICE=.*/export TAG_SCC_LOFSELECTCLUB_SERVICE=${BUILD_NAME}/" .env 2>&1
 
 echo "update .env file ..."
+
+git config user.email "anthony.denecheau@centrale-canine.fr"
+git config user.name "${GITHUB_USER}"
 git add .
-git commit -m":lock: changed version application" .
+git commit -m":rocket: :wrench: :arrow_up: changed application version" .
 git push
 
-echo "push to branch re7..."
-git checkout -b re7
-git pull origin re7
-git merge origin/master
+#echo "push to branch re7..."
+#git checkout -b re7
+#git pull origin re7
+#git merge origin/master
+#
+#git push -u origin re7
 
-git push -u origin re7
