a9cded9a6f358eef52b7363094b8ef6e305e1856
gcloud-repo <anthony.denecheau@centrale-canine.fr>
2019-12-05 14:34:28 +0100

    :bug:

--- scripts/travis/deploy_to_gcp.sh
+++ scripts/travis/deploy_to_gcp.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
-echo "checking secret env. variables..."
-if [ "q${github_user}" -eq "q" ] || [ "q${github_token} -eq "q" ]
-then
-  echo "ERROR! Variables github_user or github_token are empty!"
-  exit 204
-fi 
-
+export GITHUB_USER=gcloud-repo
 export GITHUB_REPO=github.com/centrale-canine/gcp-migration.git
+export LOCAL_REPO=../gcp-migration
+
+echo "checking secret env. variables..."
+
+if [ "q${GITHUB_TOKEN}" == "q" ]
+then
+  echo "ERROR! Secret variable GITHUB_TOKEN is empty!"
+  exit 204
+fi
 
 echo "clone git project: ${GITHUB_REPO}..."
 
-git clone https://${github_user}:${github_token}@${GITHUB_REPO} 2>&1
+git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@${GITHUB_REPO} ${LOCAL_REPO} 2>&1
 
-cd terraform/code
+cd ${LOCAL_REPO}/terraform/code
 
 echo "update .env file ..."
 sed -i "s/export TAG_SCC_LOFSELECTCLUB_SERVICE=.*/export TAG_SCC_LOFSELECTCLUB_SERVICE=${BUILD_NAME}/" .env 2>&1
 
 echo "push .env file ..."
 git config user.email "anthony.denecheau@centrale-canine.fr"
-git config user.name "${github_user}"
+git config user.name "${GITHUB_USER}"
 git add .
 git commit -m":rocket: :wrench: :arrow_up: changed application version" .
 git push
