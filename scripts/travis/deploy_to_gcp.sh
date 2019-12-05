dc1dd0ebe338cf942859de38f2d9dcbb8e7452d1
gcloud-repo <anthony.denecheau@centrale-canine.fr>
2019-12-05 14:14:19 +0100

    :rocket: :wrench: :arrow_up: changed application version

--- scripts/travis/deploy_to_gcp.sh
+++ scripts/travis/deploy_to_gcp.sh
@@ -2,6 +2,7 @@
 
 export GITHUB_USER=gcloud-repo
 export GITHUB_REPO=github.com/centrale-canine/gcp-migration.git
+export LOCAL_REPO=../gcp-migration
 
 echo "checking secret env. variables..."
 
@@ -13,9 +14,9 @@ fi
 
 echo "clone git project: ${GITHUB_REPO}..."
 
-git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@${GITHUB_REPO} 2>&1
+git clone https://${GITHUB_USER}:${GITHUB_TOKEN}@${GITHUB_REPO} ${LOCAL_REPO} 2>&1
 
-cd terraform/code
+cd ${LOCAL_REPO}}/terraform/code
 
 echo "update .env file ..."
 sed -i "s/export TAG_SCC_LOFSELECTCLUB_SERVICE=.*/export TAG_SCC_LOFSELECTCLUB_SERVICE=${BUILD_NAME}/" .env 2>&1
